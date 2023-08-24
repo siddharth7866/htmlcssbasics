19e8dea395a113dfb61260b9ae5bcde6a72ca27b
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-08-24 11:44:31 +0530

    fix

--- index.js
+++ index.js
@@ -3,21 +3,31 @@
 //shortcircuiting
 
 var sid = document.getElementById("sid");
-const getAdvice = async function () {
-  const resdata = await fetch("https://api.adviceslip.com/advice");
-  const data = await resdata.json();
 
-  // Updating values
+async function getAdvice() {
+  const url = await fetch("https://api.adviceslip.com/advice");
+  const data = await url.json();
+  console.log(data);
   var advice = data.slip.advice;
-  var count = count + 1;
+  //   var count = count + 1;
 
   sid.textContent = advice;
+}
+// const getAdvice = async function () {
+//   const resdata = await fetch("https://api.adviceslip.com/advice");
+//   const data = await resdata.json();
 
-  //   return advice;
+//   // Updating values
+//   var advice = data.slip.advice;
+//   var count = count + 1;
 
-  // Manually updating DOM elements
-  //   countEl.textContent = count;
-  //   adviceEl.textContent = advice;
-};
+//   sid.textContent = advice;
 
+//   //   return advice;
+
+//   // Manually updating DOM elements
+//   //   countEl.textContent = count;
+//   //   adviceEl.textContent = advice;
+// };
+getAdvice();
 setInterval(getAdvice, 5000);
