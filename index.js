032e62102dfb954b9fdad92be3d3b1a19032eeac
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-08-24 08:47:38 +0530

    async API

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 // alert("connected");
 
 //shortcircuiting
+
+var sid = document.getElementById("sid");
+const getAdvice = async function () {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+
+  // Updating values
+  var advice = data.slip.advice;
+  var count = count + 1;
+
+  sid.textContent = advice;
+
+  //   return advice;
+
+  // Manually updating DOM elements
+  //   countEl.textContent = count;
+  //   adviceEl.textContent = advice;
+};
+
+console.log(getAdvice());
